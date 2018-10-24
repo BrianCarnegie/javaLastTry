5ff0b3b91a21c89c83d0417963fc809bf97f0d82
bcc14 <bcc14@FSTU603033.pti.edu>
2018-10-24 16:15:36 -0400

    first

---
+++ src/javaapp2/JavaApp2.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapp2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author bcc14
+ */
+public class JavaApp2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int number = 0;
+        
+        System.out.print("Enter in a number and we will multiply it by 2 >> ");
+        number = input.nextInt();
+        
+        int solution = number * 2;
+        System.out.println("The answer is " + solution);
+    }
+    
+}
